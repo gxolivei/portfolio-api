b10f0439866d5518fecfda9992473a01950b162f
Gabriel Oliveira <ateliergabdias@gmail.com>
2023-06-23 00:06:33 -0300

    Project model. User association. Routes :projects and Controller

---
+++ db/migrate/20230623030316_create_projects.rb
@@ -0,0 +1,12 @@
+class CreateProjects < ActiveRecord::Migration[7.0]
+  def change
+    create_table :projects do |t|
+      t.references :user, null: false, foreign_key: true
+      t.string :title
+      t.text :description
+      t.string :image_url
+
+      t.timestamps
+    end
+  end
+end
